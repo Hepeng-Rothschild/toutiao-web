83f40439b76e6872225c2377776292eae70db893
magicstf <745555655@qq.com>
2019-12-02 00:42:04 +0800

    完成搜索历史记录需求

--- src/views/Search/index.vue
+++ src/views/Search/index.vue
@@ -11,19 +11,19 @@
     <!-- /搜索框 -->
 
     <!-- 联想建议 -->
-    <van-cell-group>
+    <van-cell-group v-if="suggestions.length">
       <van-cell icon="search"
                 v-for="item in suggestions"
                 :key="item"
-                @click="onSearch(item)"
-                >
-        <span slot="title" v-html="highlight(item)"></span>
+                @click="onSearch(item)">
+        <span slot="title"
+              v-html="highlight(item)"></span>
       </van-cell>
     </van-cell-group>
     <!-- /联想建议 -->
 
     <!-- 历史记录 -->
-    <van-cell-group>
+    <van-cell-group v-else>
       <van-cell title="历史记录">
         <span style="margin-right: 10px;">全部删除</span>
         <span>完成</span>
@@ -31,9 +31,9 @@
                   name="delete"
                   style="line-height: inherit;" />
       </van-cell>
-      <van-cell title="hello"
-                v-for="value in 5"
-                :key="value">
+      <van-cell :title="item"
+                v-for="item in searchHistories"
+                :key="item">
         <van-icon slot="right-icon"
                   name="close"
                   style="line-height: inherit;" />
@@ -51,29 +51,45 @@ export default {
   data () {
     return {
       searchText: '',
-      suggestions: []
+      suggestions: [],
+      searchHistories: JSON.parse(window.localStorage.getItem('search-histories')) || []
     }
   },
   watch: {
     searchText: debounce(async function (newVal) {
       if (!newVal.trim().length) {
+        this.suggestions = []
         return
       }
       const { data } = await getSuggestion(newVal)
       this.suggestions = data.data.options
-    }, 500)
+    }, 500),
+    searchHistories () {
+      window.localStorage.setItem('search-histories', JSON.stringify(this.searchHistories))
+    }
   },
   methods: {
     onSearch (q) {
+      // 记录搜索历史记录
+      const { searchHistories } = this
+      // 定义搜索历史记录的索引
+      const index = searchHistories.findIndex(item => item === q)
+      // 如果索引不等于-1，则替换页历史记录数组中的数据
+      if (index !== -1) {
+        searchHistories.splice(index, 1)
+      }
+      // 向搜索历史记录数组的0号索引添加最近的历史记录
+      this.searchHistories.unshift(q)
+      // 控制搜索跳转
       this.$router.push({
-        name:'search-result',
-        params:{
+        name: 'search-result',
+        params: {
           q
         }
       })
-     },
+    },
     onCancel () { },
-    highlight(str) {
+    highlight (str) {
       const keyword = this.searchText
       return str.toLowerCase().split(keyword).join(`<span style="color:red;">${keyword}</span>`)
     }
